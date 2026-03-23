301373f8e677970779bf623c24b07870635d9509
azizbekqiyomov55555-dev <azizbekqiyomov55555@gmail.com>
2026-03-23 21:53:33 +0500

    Update and rename bot-2.py to bot.py

--- bot.py
+++ bot.py
@@ -17,8 +17,8 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.filters import CommandStart, Command
 
 # ================== SOZLAMALAR ==================
-BOT_TOKEN = "8674741228:AAGHzR8sfFTAW9N6BpYuVWusz_cIsxAqz_w"
-ADMIN_ID = 8537782289
+BOT_TOKEN = "8632541339:AAHltUEVgFkRxjYmzfJBuORXqb_D21zptlc"
+ADMIN_ID = 7973934849
 MAIN_CHANNEL_ID = "@Azizbekl2026"
 # ================================================
 
